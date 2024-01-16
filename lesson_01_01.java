ac41a35b874bc9dc666faf708df2ebf55dedea93
ColotVS <kolotilov98@mail.ru>
2024-01-16 16:15:56 +0700

    Первая программа на Java. урок 1, задача 1

--- lesson_01_01.java
+++ lesson_01_01.java
@@ -1,6 +1,7 @@
 public class lesson_01_01 {
     public static void main(String[] args) {
         //Первая программа на Java
-        System.out.println("First pr Java Git!");
+        System.out.print("First pr ");
+        System.out.println("Java Git!");
     }
 }
