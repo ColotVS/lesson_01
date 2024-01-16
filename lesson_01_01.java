f5b47a6f51c7e62ea98eb06fac342bea8401e450
ColotVS <kolotilov98@mail.ru>
2024-01-16 16:11:00 +0700

    Комментарии в Java задача 5

--- lesson_01_01.java
+++ lesson_01_01.java
@@ -1,5 +1,6 @@
 public class lesson_01_01 {
     public static void main(String[] args) {
+        //Первая программа на Java
         System.out.println("First pr Java Git!");
     }
 }
--- lesson_01_04.java
+++ lesson_01_04.java
@@ -1,5 +1,6 @@
 public class lesson_01_04 {
     public static void main(String[] args) {
+        //Конкатенация разных переменных
         int a = 2024;
         System.out.println("Год моего рождения: " + (a-26));
     }
