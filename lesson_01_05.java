03e6319c1a1f823d62ed48cf4262b73b89169c8b
ColotVS <kolotilov98@mail.ru>
2024-01-16 16:12:03 +0700

    Комментарии в Java задача 5

--- lesson_01_05.java
+++ lesson_01_05.java
@@ -1,2 +1,15 @@
 public class lesson_01_05 {
+    public static void main(String[] args) {
+        // Выводим в консоль Happy New Year
+        // String s = "Merry";
+        // System.out.println("Christmas");
+        // System.out.println("Merry New");
+        // System.out.println("weekend");
+        System.out.print("Happy New");
+        // System.out.println("weekend");
+        // System.out.println(s);
+        // System.out.print("Merry New!");
+        System.out.print(" ");
+        System.out.println("Year");
+    }
 }
