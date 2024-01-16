57363fdfa3d10146894fa2a3c1e9957b9ddc373e
ColotVS <kolotilov98@mail.ru>
2024-01-16 16:18:36 +0700

    Инициализация переменных Java. урок 1, задача 3

--- lesson_01_03.java
+++ lesson_01_03.java
@@ -1,6 +1,8 @@
 public class lesson_01_03 {
     public static void main(String[] args) {
-        //Объявление переменных
+        //Инициализация переменных
         int a = 5, b = 3;
+        int x = a+b;
+        System.out.println(x);
     }
 }
