1a31784853d48610f7595d8290d5dfaf14aecf18
ColotVS <kolotilov98@mail.ru>
2024-01-16 16:20:13 +0700

    Слияние переменных Java. урок 1, задача 4

--- lesson_01_04.java
+++ lesson_01_04.java
@@ -1,7 +1,9 @@
 public class lesson_01_04 {
     public static void main(String[] args) {
-        //Конкатенация разных переменных
+        //Слияние разных переменных
         int a = 2024;
-        System.out.println("Год моего рождения: " + (a-26));
+        int b = 26;
+        int c = a+b;
+        System.out.println("Год моего рождения: " + (c-b-b));
     }
 }
