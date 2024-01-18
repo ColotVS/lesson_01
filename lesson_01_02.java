bf908f654f689c7a77dbdbf48e12ba4a26041e8c
ColotVS <kolotilov98@mail.ru>
2024-01-18 15:59:51 +0700

    Создание переменных Урок 1, задача 2

---
+++ lesson_01_02.java
@@ -0,0 +1,9 @@
+public class lesson_01_02 {
+    public static void main(String[] args) {
+        //Создание переменных
+        String name;
+        int i;
+        long x;
+        boolean no = false;
+    }
+}
