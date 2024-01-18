ca5833a42e2476a7cf673829b883708072dce49d
ColotVS <kolotilov98@mail.ru>
2024-01-18 16:02:34 +0700

    Слияние разных переменных Урок 1, задача 4

---
+++ lesson_01_04.java
@@ -0,0 +1,9 @@
+public class lesson_01_04 {
+    public static void main(String[] args) {
+        //Слияние разных переменных
+        int a = 2024;
+        int b = 26;
+        int c = a+b;
+        System.out.println("Год моего рождения: " + (c-b-b));
+    }
+}
